bf5a349065b6333b6ff0da8551052906b7a65eb8
sorinam12 <98848690+sorinam12@users.noreply.github.com>
2022-12-07 18:47:16 +0200

    AirportForm

---
+++ AirportForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicatie
+{
+    public partial class AirportForm : Form
+    {
+        public AirportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminForm Pilot = new AdminForm();
+            Pilot.Show();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AirportForm Airport = new AirportForm();
+            Airport.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FlightForm Flight = new FlightForm();
+            Flight.Show();
+        }
+    }
+}
